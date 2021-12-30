28453463a7f8a800433269a22de31007e54078c8
Albert Chang <albert_w_chang@apple.com>
2021-12-30 03:08:05 -0800

    corrections to directory structure

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const fastify = require('fastify')({ logger: true });
+
+fastify.get('/', async (req, res) => ({ hello: 'world' }));
+
+async function start() {
+  try {
+    await fastify.listen(3000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start();
